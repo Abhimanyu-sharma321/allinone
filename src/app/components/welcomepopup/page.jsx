c9976129e985b02e6f82f68f6bb66610d0c89094
abhimanyu <abhimanyu-sharma@csgroupchd.com>
2024-12-13 18:39:45 +0530

    first-push

---
+++ src/app/components/welcomepopup/page.jsx
@@ -0,0 +1,19 @@
+const WelcomePopup = () => {
+    return (
+        <div className="welcomePopup">
+
+            <div style={{ background: "white", color: "skyblue", border: "groove" }}>
+
+                <h2>Welcome</h2>
+
+
+
+
+            </div>
+
+
+        </div>
+    )
+}
+
+export default WelcomePopup
---
+++ src/app/home/page.tsx
@@ -0,0 +1,9 @@
+const Landing = () => {
+  return (
+    <div className="">
+      <h2 className="welcome-heading" style={{alignItems:"center",marginLeft:"800px"}}>Welcome</h2>
+    </div>
+  );
+};
+
+export default Landing;
